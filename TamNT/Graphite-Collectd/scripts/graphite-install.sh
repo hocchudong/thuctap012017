486072f25c45066695dc93826b7822b0d08c6ff7
ThanhTamPotter <thanhtampotter285@gmail.com>
2018-03-29 17:11:14 +0700

    update scripts install Graphite + Collectd

--- TamNT/Graphite-Collectd/scripts/graphite-install.sh
+++ TamNT/Graphite-Collectd/scripts/graphite-install.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 ## Chinh sua cac tham so sau tuy theo mo hinh
-PASS_TEST=123456
+PASS_GRAPHITE_DB=123456
 SECRET_KEY='anything_that_you_think'
 TIME_ZONE=Asia/Ho_Chi_Minh
+graphite_user_name=graphite_user
+graphite_db_name=graphite_db
 ip_graphite=10.10.10.10
 
 echo "#######Install Graphite-server#######"
@@ -12,8 +14,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y install graphite-web graphite-
 apt-get install postgresql libpq-dev python-psycopg2 -y
 echo "Config graphite"
 cat << EOF | sudo -u postgres psql
-CREATE USER graphite_user WITH PASSWORD '$PASS_TEST';
-CREATE DATABASE graphite_db WITH OWNER graphite_user;
+CREATE USER $graphite_user_name WITH PASSWORD '$PASS_GRAPHITE_DB';
+CREATE DATABASE $graphite_db_name WITH OWNER $graphite_user_name;
 \q
 EOF
 graphite_local=/etc/graphite/local_settings.py
@@ -34,16 +36,15 @@ INDEX_FILE = '/var/lib/graphite/search_index'  # Search index file
 USE_REMOTE_USER_AUTHENTICATION = True
 DATABASES = {
 'default': {
-   'NAME': 'graphite_db',
+   'NAME': '$graphite_db_name',
    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   'USER': 'graphite_user',
+   'USER': '$graphite_user_name',
    'PASSWORD': '$PASS_TEST', 
    'HOST': 'localhost',
    'PORT': ''
    }
 }
 EOF
-
 graphite-manage migrate auth
 graphite-manage syncdb --noinput
 cp /etc/default/graphite-carbon /etc/default/graphite-carbon.orig
